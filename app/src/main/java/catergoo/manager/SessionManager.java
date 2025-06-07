2eda1e8468a8564146bede3c77e4c58943f8e3ca
chxnsa <anisaandi1980@gmail.com>
2025-06-07 22:15:21 +0800

    feat: menambahkan atribut dan method logout & getCurrentUser

--- app/src/main/java/catergoo/manager/SessionManager.java
+++ app/src/main/java/catergoo/manager/SessionManager.java
@@ -1,5 +1,20 @@
 package catergoo.manager;
 
-public class SessionManager {
+import catergoo.model.User;
+import java.util.List;
+import java.util.ArrayList;
 
+public class SessionManager {
+    private static List<User> registeredUsers = new ArrayList<>();
+    private static User currentUser = null;
+
+    public static void logoutUser() {
+        currentUser = null;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    // TODO : menambahkan method isUserRegistered, registerUser dan loginUser.
 }
