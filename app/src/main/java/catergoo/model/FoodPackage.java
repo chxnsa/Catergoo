7384b7f508b927c63bd2c826783154f11df9cd26
llLanisterll <sarhamkampus@gmail.com>
2025-06-09 20:53:37 +0800

    feat: menambahkan constructor FoodPackage dengan parameter kustomisasi

--- app/src/main/java/catergoo/model/FoodPackage.java
+++ app/src/main/java/catergoo/model/FoodPackage.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
  * Mengimplementasikan interface Customizable untuk mendukung kustomisasi
  */
 
-public class FoodPackage {
+public class FoodPackage extends MenuItem implements Customizeable {
     // Daftar pilihan kustomisasi
     private List<String> vegetableOptions;  // Pilihan sayur (Capcay, Tumis Buncis, dll)
     private List<String> sideDishOptions;   // Pilihan lauk tambahan (Tempe Oreg, Perkedel)
@@ -21,4 +21,28 @@ public class FoodPackage {
     private String selectedSideDish;
     private String selectedFruit;
     private boolean selectedIncludeCrackers;
+
+    /**
+     * Constructor untuk inisialisasi paket makanan
+     * @param itemName Nama paket
+     * @param pricePerPax Harga per porsi
+     * @param minOrder Minimal pemesanan
+     * @param description Deskripsi paket
+     * @param imagePath Path gambar paket
+     * @param vegetableOptions List pilihan sayur
+     * @param sideDishOptions List pilihan lauk tambahan
+     * @param fruitOptions List pilihan buah
+     * @param includeCrackers Flag default untuk kerupuk
+     */
+    public FoodPackage(String itemName, double pricePerPax, int minOrder, 
+                      String description, String imagePath,
+                      List<String> vegetableOptions, List<String> sideDishOptions, 
+                      List<String> fruitOptions, boolean includeCrackers) {
+        // Memanggil constructor parent class (MenuItem)
+        super(itemName, pricePerPax, minOrder, description, imagePath, true);
+        this.vegetableOptions = vegetableOptions;
+        this.sideDishOptions = sideDishOptions;
+        this.fruitOptions = fruitOptions;
+        this.includeCrackers = includeCrackers;
+    }
 }
