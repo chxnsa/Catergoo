eb1da24af6879b056c29924d1afa32e2664d7200
llLanisterll <sarhamkampus@gmail.com>
2025-06-09 13:31:47 +0800

    fix: memperbaiki interface Customizable

--- app/src/main/java/catergoo/model/Customizeable.java
+++ app/src/main/java/catergoo/model/Customizeable.java
@@ -1,18 +1,22 @@
 package catergoo.model;
-import java.time.LocalDate;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
+
+/**
+ * Interface untuk item menu yang bisa dikustomisasi
+ */
 
 public interface Customizeable {
     // Mengembalikan panel kustomisasi (diimplementasikan di JavaFX)
     // Node getCustomizationPanel();
     
-    // Menerapkan kustomisasi yang dipilih
+    /**
+     * Menerapkan kustomisasi yang dipilih
+     * @param selections Map berisi pilihan kustomisasi
+     */
     void applyCustomizations(Map<String, String> selections);
     
-    // Mengembalikan deskripsi kustomisasi yang dipilih
+    /**
+     * @return Deskripsi kustomisasi yang dipilih
+     */
     String getSelectedCustomizations();
 }
