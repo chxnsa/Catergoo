0865fc9633cd175b30951cc3217f9cdfc12c9a28
AkmalHstl <akmalrbc6@gmail.com>
2025-06-09 19:31:28 +0800

    feat: menambahkan method tambah dan hapus item, clear cart, menambah pesanan ke riwayat pesanan

--- app/src/main/java/catergoo/model/User.java
+++ app/src/main/java/catergoo/model/User.java
@@ -19,4 +19,58 @@ public class User {
     this.currentCart = new ArrayList<>();
     this.orderHistory = new ArrayList<>();
   }
+
+  // --- Method untuk mengelola keranjang dan riwayat ---
+
+  /**
+   * Menambahkan item ke keranjang belanja.
+   * 
+   * @param item Item yang akan ditambahkan.
+   */
+  public void addToCart(CartItem item) {
+    this.currentCart.add(item);
+  }
+
+  /**
+   * Menghapus item dari keranjang belanja.
+   * 
+   * @param item Item yang akan dihapus.
+   */
+  public void removeFromCart(CartItem item) {
+    this.currentCart.remove(item);
+  }
+
+  /**
+   * Mengosongkan keranjang belanja.
+   */
+  public void clearCart() {
+    this.currentCart.clear();
+  }
+
+  /**
+   * Menambahkan pesanan ke riwayat pesanan.
+   * 
+   * @param order Pesanan yang telah dikonfirmasi.
+   */
+  public void addToHistory(Order order) {
+    this.orderHistory.add(order);
+  }
+
+  // --- Getters ---
+  // Getter untuk semua atribut
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public List<CartItem> getCurrentCart() {
+    return currentCart;
+  }
+
+  public List<Order> getOrderHistory() {
+    return orderHistory;
+  }
 }
