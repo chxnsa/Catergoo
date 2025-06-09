fa0ea7dd3c8d79d7898a7a8c077accc5c19b1a6a
AkmalHstl <akmalrbc6@gmail.com>
2025-06-09 19:23:14 +0800

    feat: menambahkan constructor dan getters pada class order

--- app/src/main/java/catergoo/model/Order.java
+++ app/src/main/java/catergoo/model/Order.java
@@ -14,4 +14,61 @@ public class Order {
   private String paymentMethod;
   private String paymentProofPath;
   private String status;
+
+  /**
+   * Constructor untuk kelas Order.
+   */
+  public Order(String orderId, User user, List<CartItem> items, double totalAmount, String deliveryAddress,
+      String paymentMethod, String paymentProofPath) {
+    this.orderId = orderId;
+    this.user = user;
+    this.items = items;
+    this.totalAmount = totalAmount;
+    this.deliveryAddress = deliveryAddress;
+    this.paymentMethod = paymentMethod;
+    this.paymentProofPath = paymentProofPath;
+    this.orderDate = LocalDateTime.now(); // Waktu pesanan dibuat
+    this.status = "Menunggu Konfirmasi"; // Status awal
+  }
+
+  // --- Getters ---
+  public String getOrderId() {
+    return orderId;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public List<CartItem> getItems() {
+    return items;
+  }
+
+  public double getTotalAmount() {
+    return totalAmount;
+  }
+
+  public String getDeliveryAddress() {
+    return deliveryAddress;
+  }
+
+  public LocalDateTime getOrderDate() {
+    return orderDate;
+  }
+
+  public String getPaymentMethod() {
+    return paymentMethod;
+  }
+
+  public String getPaymentProofPath() {
+    return paymentProofPath;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
 }
