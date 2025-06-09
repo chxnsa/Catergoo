0ad51d84f446387cd3c6768cfa0818c01e397633
AkmalHstl <akmalrbc6@gmail.com>
2025-06-09 18:41:15 +0800

    feat: menambahkan constructor pada cart item

--- app/src/main/java/catergoo/model/CartItem.java
+++ app/src/main/java/catergoo/model/CartItem.java
@@ -10,4 +10,17 @@ public class CartItem {
   private String specialNotes;
   private String customizations;
   private double subTotal;
+
+  /**
+   * Constructor untuk menginisialisasi atribut dan menghitung subTotal.
+   */
+  public CartItem(MenuItem menuItem, int quantity, LocalDate deliveryDate, String customizations, String specialNotes) {
+    this.menuItem = menuItem;
+    this.quantity = quantity;
+    this.deliveryDate = deliveryDate;
+    this.customizations = customizations;
+    this.specialNotes = specialNotes;
+    // Menghitung subtotal saat objek dibuat
+    this.subTotal = menuItem.calculatePrice(quantity);
+  }
 }
