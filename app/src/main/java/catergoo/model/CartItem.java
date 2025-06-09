ac639360bb93bb02d81311918884d7f0449e1f62
AkmalHstl <akmalrbc6@gmail.com>
2025-06-09 18:45:33 +0800

    feat: menambahkan method updateQuantity dan getters untuk semua atribut

--- app/src/main/java/catergoo/model/CartItem.java
+++ app/src/main/java/catergoo/model/CartItem.java
@@ -23,4 +23,37 @@ public class CartItem {
     // Menghitung subtotal saat objek dibuat
     this.subTotal = menuItem.calculatePrice(quantity);
   }
+
+  /**
+   * Mengupdate kuantitas dan menghitung ulang subtotal.
+   */
+  public void updateQuantity(int newQuantity) {
+    this.quantity = newQuantity;
+    this.subTotal = this.menuItem.calculatePrice(newQuantity);
+  }
+
+  // --- Getters untuk semua atribut ---
+  public MenuItem getMenuItem() {
+    return menuItem;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public LocalDate getDeliveryDate() {
+    return deliveryDate;
+  }
+
+  public String getSpecialNotes() {
+    return specialNotes;
+  }
+
+  public String getCustomizations() {
+    return customizations;
+  }
+
+  public double getSubTotal() {
+    return subTotal;
+  }
 }
