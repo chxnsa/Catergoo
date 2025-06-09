4bd4605c7e3e2d34b5c7c85389d24f45b29b1569
AkmalHstl <akmalrbc6@gmail.com>
2025-06-09 18:22:06 +0800

    feat: menambahkan fitur cart item

--- app/src/main/java/catergoo/model/CartItem.java
+++ app/src/main/java/catergoo/model/CartItem.java
@@ -1,5 +1,13 @@
 package catergoo.model;
 
-public class CartItem {
+import java.time.LocalDate;
 
+public class CartItem {
+  // Atribut dari kelas CartItem
+  private MenuItem menuItem;
+  private int quantity;
+  private LocalDate deliveryDate;
+  private String specialNotes;
+  private String customizations;
+  private double subTotal;
 }
