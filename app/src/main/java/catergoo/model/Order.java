f46664266c5453f30b031670ad5c3634fa690595
AkmalHstl <akmalrbc6@gmail.com>
2025-06-09 19:21:48 +0800

    feat: menambahkan atribute dari class order

--- app/src/main/java/catergoo/model/Order.java
+++ app/src/main/java/catergoo/model/Order.java
@@ -4,5 +4,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Order {
-
+  // Atribut dari kelas Order
+  private String orderId;
+  private User user; // Referensi ke pengguna yang memesan
+  private List<CartItem> items;
+  private double totalAmount;
+  private String deliveryAddress;
+  private LocalDateTime orderDate;
+  private String paymentMethod;
+  private String paymentProofPath;
+  private String status;
 }
