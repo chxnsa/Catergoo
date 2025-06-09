68a48040cf4bfad18a6927a1ea117d9609e151dd
llLanisterll <sarhamkampus@gmail.com>
2025-06-09 13:21:04 +0800

    feat: menambahkan interface Customizable

--- app/src/main/java/catergoo/model/Customizeable.java
+++ app/src/main/java/catergoo/model/Customizeable.java
@@ -1,5 +1,18 @@
 package catergoo.model;
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public interface Customizeable {
-
+    // Mengembalikan panel kustomisasi (diimplementasikan di JavaFX)
+    // Node getCustomizationPanel();
+    
+    // Menerapkan kustomisasi yang dipilih
+    void applyCustomizations(Map<String, String> selections);
+    
+    // Mengembalikan deskripsi kustomisasi yang dipilih
+    String getSelectedCustomizations();
 }
