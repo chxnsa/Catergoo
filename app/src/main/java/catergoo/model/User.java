168944adabe1ce8e326d7b08f91df7d9772e9857
AkmalHstl <akmalrbc6@gmail.com>
2025-06-09 19:27:32 +0800

    feat: menambahkan atribut dan constructor dari class user

--- app/src/main/java/catergoo/model/User.java
+++ app/src/main/java/catergoo/model/User.java
@@ -4,5 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
+  // Atribut dari kelas User
+  private String username;
+  private String password;
+  private List<CartItem> currentCart; // Keranjang pribadi user
+  private List<Order> orderHistory; // Riwayat pesanan user
 
+  /**
+   * Constructor untuk kelas User.
+   */
+  public User(String username, String password) {
+    this.username = username;
+    this.password = password;
+    this.currentCart = new ArrayList<>();
+    this.orderHistory = new ArrayList<>();
+  }
 }
